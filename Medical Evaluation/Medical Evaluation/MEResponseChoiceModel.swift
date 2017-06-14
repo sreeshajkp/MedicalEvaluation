38414c10a18f568e6625213f3c84ca448742fff6
Musadhikh Muhammed K <musadhikh@inapp.com>
2017-06-14 17:02:21 +0530

    model of response choice id

---
+++ Medical Evaluation/Medical Evaluation/MEResponseChoiceModel.swift
@@ -0,0 +1,68 @@
+//
+//  MEResponseChoiceModel.swift
+//  Medical Evaluation
+//
+//  Created by Veena on 14/06/17.
+//  Copyright © 2017 Sreeshaj Kp. All rights reserved.
+//
+
+import Foundation
+
+class MEResponseChoiceModel {
+
+    var section : MESection?
+    var responseChoiceId : Int?
+    var text : String?
+    var comment : String?
+    var createdDate : String?
+    
+    init(values: NSDictionary){
+        if let evaluationDict = values["Section"] as? NSDictionary{
+            self.section = MESection(values: evaluationDict)
+        }
+        self.responseChoiceId = values["ResponseChoiceId"] as? Int
+        self.text = values["Text"] as? String
+        self.comment = values["Comment"] as? String
+        self.createdDate = values["CreatedDate"] as? String
+    }
+}
+
+class MESection{
+    var mSectionId : Int?
+    var mName : String?
+    var mDescription : String?
+    var mCreatedDate : Bool?
+    var mLastUpdatedDate : Int?
+    var questionList = [MEQuestionModel]()
+     var evaluationList : MEEvaluations?
+    var mIsDelete : Bool?
+    
+    init(values: NSDictionary){
+        self.mSectionId = values["SectionId"] as? Int
+        self.mName = values["Name"] as? String
+        self.mDescription = values["Description"] as? String
+        self.mCreatedDate = values["CreatedDate"] as? Bool
+        self.mLastUpdatedDate = values["LastUpdatedDate"] as? Int
+        self.mIsDelete = values["IsDelete"] as? Bool
+        if let questionListDict = values["QuestionList"] as? NSArray{
+            questionList = fetchingMemberList(questionListDict)
+        }
+        if let evaluationDict = values["Evaluation"] as? NSDictionary{
+            self.evaluationList = MEEvaluations(values: evaluationDict)
+        }
+    }
+    
+    func fetchingMemberList(memberArray: NSArray) -> [MEQuestionModel]{
+        if memberArray.count != 0{
+            for each in memberArray as! [NSDictionary]{
+                let memberList = MEQuestionModel(values: each)
+                let _ = self.questionList
+                self.questionList.append(memberList)
+            }
+        }
+        print(questionList)
+        return questionList
+    }
+}
+
+
